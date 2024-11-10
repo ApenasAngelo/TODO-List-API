f0052704764057dddab6c6bd3e7e683c1182e1da
Angelo <mangelogm28@gmail.com>
2024-11-10 17:48:59 -0300

    Implementado segurança Implementação básica de autenticação usando Spring Security

--- src/main/java/com/apenasnagelo/api_rest/Controller/WelcomeController.java
+++ src/main/java/com/apenasnagelo/api_rest/Controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.apenasnagelo.api_rest.Controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,15 @@ public class WelcomeController {
     @GetMapping
     public String Welcome() {
         return "Welcome to my Spring Boot Web API";
+    }
+
+    @GetMapping("/user")
+    public String users() {
+        return "Authorized user";
+    }
+
+    @GetMapping("/admin")
+    public String admins() {
+        return "Authorized admin";
     }
 }
